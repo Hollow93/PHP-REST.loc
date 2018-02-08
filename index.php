3e6623f87e9690ecb76531d80efc05da4f7a685e
Hollow93 <bv@online-gymnasium.ru>
2018-02-08 16:16:54 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vladislav
+ * Date: 08.02.2018
+ * Time: 14:06
+ */
+error_reporting(E_ALL);
+mb_internal_encoding("UTF-8");
+
+echo 'Connect moodle...';
+
+require_once('./client.php');
