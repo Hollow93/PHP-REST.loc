d1a7654f186f2953f5053df5c6e353b15e8c8ff0
Hollow93 <bv@online-gymnasium.ru>
2018-02-15 17:53:11 +0300

    nvalid parameter: cohortype=string

--- client.php
+++ client.php
@@ -15,9 +15,12 @@
  */
 
 /// SETUP - NEED TO BE CHANGED
-$token = 'acabec9d20933913f14301785324f579';
-$domainname = 'http://m311t2.com';
-$functionname = 'core_user_create_users';
+$token = '2bbc8414d1b551dfc834c5d88aef8688';
+$domainname = 'https://test.online-gymnasium.ru';
+$create_users = 'core_user_create_users';
+$get_cohort_members = 'core_cohort_add_cohort_members';
+
+
 
 // REST RETURNED VALUES FORMAT
 $restformat = 'json'; //Also possible in Moodle 2.2 and later: 'json'
@@ -26,42 +29,51 @@ $restformat = 'json'; //Also possible in Moodle 2.2 and later: 'json'
 //////// moodle_user_create_users ////////
 
 /// PARAMETERS - NEED TO BE CHANGED IF YOU CALL A DIFFERENT FUNCTION
-$user1 = new stdClass();
-$user1->username = 'testusername1';
-$user1->password = 'testpassword1';
-$user1->firstname = 'testfirstname1';
-$user1->lastname = 'testlastname1';
-$user1->email = 'testemail1@moodle.com';
-$user1->auth = 'manual';
-$user1->idnumber = 'testidnumber1';
-$user1->lang = 'en';
-$user1->theme = 'standard';
-$user1->timezone = '-12.5';
-$user1->mailformat = 0;
-$user1->description = 'Hello World!';
-$user1->city = 'testcity1';
-$user1->country = 'au';
-$preferencename1 = 'preference1';
-$preferencename2 = 'preference2';
-$user1->preferences = array(
-    array('type' => $preferencename1, 'value' => 'preferencevalue1'),
-    array('type' => $preferencename2, 'value' => 'preferencevalue2'));
+//$user1 = new stdClass();
+//$user1->username = 'testusername1';
+//$user1->password = 'testpassword1';
+//$user1->firstname = 'testfirstname1';
+//$user1->lastname = 'testlastname1';
+//$user1->email = 'testemail1@moodle.com';
+//$user1->auth = 'manual';
+//$user1->idnumber = 'testidnumber1';
+//$user1->lang = 'en';
+//$user1->theme = 'standard';
+//$user1->timezone = '-12.5';
+//$user1->mailformat = 0;
+//$user1->description = 'Hello World!';
+//$user1->city = 'testcity1';
+//$user1->country = 'au';
+//$preferencename1 = 'preference1';
+//$preferencename2 = 'preference2';
+//$user1->preferences = array(
+//    array('type' => $preferencename1, 'value' => 'preferencevalue1'),
+//    array('type' => $preferencename2, 'value' => 'preferencevalue2'));
 $user2 = new stdClass();
-$user2->username = 'testusername2';
-$user2->password = 'testpassword2';
-$user2->firstname = 'testfirstname2';
-$user2->lastname = 'testlastname2';
-$user2->email = 'testemail2@moodle.com';
+$user3 = new stdClass();
+$user2->username = 'testusername7';
+$user2->password = 'Pendalf121#';
+$user2->firstname = 'testfirstname7';
+$user2->lastname = 'testlastname7';
+$user2->email = 'testemail7@moodle.com';
 $user2->timezone = 'Pacific/Port_Moresby';
-$users = array($user1, $user2);
-$params = array('users' => $users);
+$users = array($user2);
+$paramsUser = array('users' => $users);
+
+$members[0]['cohorttype']['type']= 'cohortid' ;
+$members[0]['cohorttype']['value']= '100';
+$members[0]['usertype']['type']= 'userid';
+$members[0]['usertype']['value']= '3';
+
+$paramsCohor=  array('members'=>$members);
 
 /// REST CALL
 header('Content-Type: text/plain');
-$serverurl = $domainname . '/webservice/rest/server.php'. '?wstoken=' . $token . '&wsfunction='.$functionname;
+$serverurl = $domainname . '/webservice/rest/server.php'. '?wstoken=' . $token . '&wsfunction='.$get_cohort_members;
 require_once('./curl.php');
 $curl = new curl;
 //if rest format == 'xml', then we do not add the param for backward compatibility with Moodle < 2.2
 $restformat = ($restformat == 'json')?'&moodlewsrestformat=' . $restformat:'';
-$resp = $curl->post($serverurl . $restformat, $params);
+//$resp  = $curl->post($serverurl . $restformat, $paramsUser);
+$resp  = $curl->post($serverurl . $restformat, $paramsCohor);
 print_r($resp);
